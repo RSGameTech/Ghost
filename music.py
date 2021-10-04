a8e9e308f19fa202927c6df9da5862b183758262
RSGameTech <72400033+RSGameTech@users.noreply.github.com>
2021-10-04 16:10:14 +0530

    Ghost

--- music.py
+++ music.py
@@ -15,6 +15,17 @@ class music(commands.Cog, name="Music"):
             await voice_channel.connect()
         else:
             await ctx.voice_client.move_to(voice_channel)
+        
+    @commands.command()
+    async def disconnect(self,ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def play(self,ctx,url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_option':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options':'-vn'}
+        YDL_OPTIONS = {'format':'bestaudio'}
+        vc = ctx.voice_client
 
 def setup(bot: commands.Bot):
     bot.add_cog(music(bot))
